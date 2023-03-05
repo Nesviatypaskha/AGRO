520c4d60b1e3762df6ee5450dfeeeba7ddaa42b8
Nesviatypaskha Oleksii <alex.nesv@gmail.com>
2023-03-05 23:37:01 +0200

    Init

---
+++ AGRO/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AGRO;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AGRO/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AGRO;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
