3aefcf8d9dcd7c5bc37ae2af571abbd5f641b92b
Nesviatypaskha Oleksii <alex.nesv@gmail.com>
2023-03-06 18:49:23 +0200

    Base imp

--- AGRO/MainPage.xaml.cs
+++ AGRO/MainPage.xaml.cs
@@ -1,24 +1,48 @@
 ﻿namespace AGRO;
 
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+using Map = Microsoft.Maui.Controls.Maps.Map;
+
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public MainPage()
+    {
+        InitializeComponent();
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+        map.IsShowingUser = true;
+        map.IsZoomEnabled = true;
+        map.MapClicked += OnMapClicked;
+    }
+    void OnMapClicked(object sender, MapClickedEventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine($"MapClick: {e.Location.Latitude}, {e.Location.Longitude}");
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+        Pin pin = new Pin
+        {
+            Label = "Santa Cruz",
+            Address = "The city with a boardwalk",
+            Type = PinType.Place,
+            Location = new Location(e.Location.Latitude, e.Location.Longitude)
+        };
+        map.Pins.Add(pin);
+    }
+    void OnButtonClicked(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+        switch (button.Text)
+        {
+            case "Street":
+                map.MapType = MapType.Street;
+                break;   
+            case "Satellite":
+                map.MapType = MapType.Satellite;
+                break;
+            case "Hybrid":
+                map.MapType = MapType.Hybrid;
+                break;
+        }
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
--- AGRO/MauiProgram.cs
+++ AGRO/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace AGRO;
+﻿using Microsoft.Extensions.Logging;
+
+namespace AGRO;
 
 public static class MauiProgram
 {
@@ -11,7 +13,12 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+			.UseMauiMaps();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
 
 		return builder.Build();
 	}
